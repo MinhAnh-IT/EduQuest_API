7b7cb43a4e1dd9cf9218546420a71fbad4995e4a
unknown <hoangchithanh23072003@gmail.com>
2025-06-24 09:02:43 +0700

    switch to new branch

---
+++ src/main/java/com/vn/EduQuest/payload/response/question/QuestionCreateResponse.java
@@ -0,0 +1,19 @@
+package com.vn.EduQuest.payload.response.question;
+
+import com.vn.EduQuest.payload.response.answer.AnswerResponse;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@FieldDefaults(level = lombok.AccessLevel.PRIVATE)
+public class QuestionCreateResponse {
+    int id;
+    String content;
+    List<AnswerResponse> answers;
+    String difficulty;
+    LocalDateTime createdAt;
+    LocalDateTime updatedAt;
+}
